a91ee01c8299d23f4dbaee22df55441e969dd171
Emmanuel-Costa <emmanuelcosta@id.uff.br>
2021-05-05 11:11:44 -0300

    Primeira aula parte inicial

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    numero = input("Digite um número: ")
+    numero2 = input("Digite outro número: ")
+    print("Os números digitados foram: {} e {}".format(numero, numero2))
+
+
